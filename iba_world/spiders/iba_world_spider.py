dc49f0ed7ef4e69c300e3e35f8d0b48c337e696b
laurence-liu <laurence.liuuu@gmail.com>
2019-04-19 00:49:18 -0700

    Import Beautiful Soup

--- iba_world/spiders/iba_world_spider.py
+++ iba_world/spiders/iba_world_spider.py
@@ -1,4 +1,6 @@
+from bs4 import BeautifulSoup
 import scrapy
+
 
 class CocktailsSpider(scrapy.Spider):
     name = "cocktails"
@@ -9,9 +11,27 @@ class CocktailsSpider(scrapy.Spider):
     ]
 
     def parse(self, response):
-        for cocktail in response.css('div.col-lg-7.col-md-6.col-sm-6'):
+        soup = BeautifulSoup(response.text, 'lxml')
+
+        cocktails = soup.find_all("div", class_="col-lg-7 col-md-6 col-sm-6")
+
+        for cocktail in cocktails:
+            title = cocktail.find("h3").get_text()
+            try:
+                pic = cocktail.find("div", class_="blog_text").find("p").find("a", href=True).get("href")
+            except AttributeError:
+                pic = ""
+            link = cocktail.find("div", class_="button_wrapper").find("a").get("href")
+
             yield {
-                'title': cocktail.xpath('h3/text()').get(),
-                'pic': cocktail.css('div.blog_text a::attr(href)').get(), 
-                'link': cocktail.css('div.button_wrapper a::attr(href)').get(),
+                'title': title,
+                'pic': pic,
+                'link': link,
             }
+
+        # for cocktail in response.css('div.col-lg-7.col-md-6.col-sm-6'):
+        #     yield {
+        #         'title': cocktail.xpath('h3/text()').get(),
+        #         'pic': cocktail.css('div.blog_text a::attr(href)').get(),
+        #         'link': cocktail.css('div.button_wrapper a::attr(href)').get(),
+        #     }
